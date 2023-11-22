efea0316bb6c09a4e10c3992ec89152f07b49489
danielntow <danielntow@gmail.com>
2023-11-22 06:00:31 +0000

    sorting algorithms in  c

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,74 +1,94 @@
 #include "sort.h"
-
-
+#include <stdio.h>
 /**
-* get_max - A utility function to get the maximum value in arr[]
-* @arr: The array
-* @size: The size of the array
-* Return: The maximum value in the array
-*/
-int get_max(int *arr, int size)
+ * swap - change two values in ascending or descending order
+ * @arr: array
+ * @item1: item one
+ * @item2: item two
+ * @order: 1: ascending order, 0 descending order
+ */
+void swap(int arr[], int item1, int item2, int order)
 {
-int i, max = arr[0];
+	int temp;
 
-for (i = 1; i < size; i++)
-	if (arr[i] > max)
-		max = arr[i];
-return (max);
+	if (order == (arr[item1] > arr[item2]))
+	{
+		temp = arr[item1];
+		arr[item1] = arr[item2];
+		arr[item2] = temp;
+	}
 }
-
 /**
-* radix_count_sort - A function to perform counting sort of arr[] based on
-* the digit represented by exp.
-* @arr: The array to be sorted
-* @size: The size of the array
-* @exp: The exponent (10^i)
-* @output: The output array to store temporary values
-*/
-void radix_count_sort(int *arr, size_t size, int exp, int *output)
+ * merge - sort bitonic sequences recursively in both orders
+ * @arr: array
+ * @low: first element
+ * @nelemnt: elements number
+ * @order: 1: ascending order, 0 descending order
+ */
+void merge(int arr[], int low, int nelemnt, int order)
 {
-int i;
-int count[10] = {0};
+	int mid, i;
 
-for (i = 0; i < (int)size; i++)
-	count[(arr[i] / exp) % 10]++;
-
-for (i = 1; i < 10; i++)
-	count[i] += count[i - 1];
-
-for (i = size - 1; i >= 0; i--)
-{
-	output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-	count[(arr[i] / exp) % 10]--;
+	if (nelemnt > 1)
+	{
+		mid = nelemnt / 2;
+		for (i = low; i < low + mid; i++)
+			swap(arr, i, i + mid, order);
+		merge(arr, low, mid, order);
+		merge(arr, low + mid, mid, order);
+	}
 }
-
-for (i = 0; i < (int)size; i++)
-	arr[i] = output[i];
-}
-
 /**
-* radix_sort - The main function to perform Radix Sort
-* @array: The array to be sorted
-* @size: The size of the array
-*/
-void radix_sort(int *array, size_t size)
+ * bitonicsort - bitonic sort algorithm implementation
+ * @arr: array
+ * @low: first element
+ * @nelemnt: number of elements
+ * @order: 1: ascending order, 0 descending order
+ * @size: array lenght
+ */
+void bitonicsort(int arr[], int low, int nelemnt, int order, int size)
 {
-int exp, max = 0;
-int *output = NULL;
+	int mid;
 
-if (array == NULL || size < 2)
-	return;
-
-max = get_max(array, size);
-output = malloc(size * sizeof(int));
-if (output == NULL)
-	return;
-
-for (exp = 1; max / exp > 0; exp *= 10)
+	if (nelemnt > 1)
+	{
+		if (order >= 1)
+		{
+			printf("Merging [%i/%i] (UP):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		else
+		{
+			printf("Merging [%i/%i] (DOWN):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		mid = nelemnt / 2;
+		bitonicsort(arr, low, mid, 1, size);
+		bitonicsort(arr, low + mid, mid, 0, size);
+		merge(arr, low, nelemnt, order);
+		if (order <= 0)
+		{
+			printf("Result [%i/%i] (DOWN):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+		if (order >= 1)
+		{
+			printf("Result [%i/%i] (UP):\n", nelemnt, size);
+			print_array(&arr[low], nelemnt);
+		}
+	}
+}
+/**
+ * bitonic_sort - prepare the terrain to bitonic sort algorithm
+ * @array: array
+ * @size: array lenght
+ */
+void bitonic_sort(int *array, size_t size)
 {
-	radix_count_sort(array, size, exp, output);
-	print_array(array, size);
+	int order = 1;
+
+	if (!array || size < 2)
+		return;
+	bitonicsort(array, 0, size, order, size);
 }
 
-free(output);
-}
